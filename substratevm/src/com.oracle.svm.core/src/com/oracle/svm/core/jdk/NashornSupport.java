jerboaa/graal

commit cc11ae1d1fad550424b1fdc0a6cf7e9d6815874a

Allow switching to builder-on-module-path on demand

substratevm/src/com.oracle.svm.driver/src/com/oracle/svm/driver/NativeImage.java
@@ -93,8 +93,8 @@ import com.oracle.svm.util.ModuleSupport;
 
 public class NativeImage {
 
-    /* Used to enable native-image JPMS support (WIP) - will become default once completed */
-    static final boolean USE_NI_JPMS = System.getenv().getOrDefault("USE_NATIVE_IMAGE_JAVA_PLATFORM_MODULE_SYSTEM", "false").equalsIgnoreCase("true");
+    private static final String ENV_VAR_USE_MODULE_SYSTEM = "USE_NATIVE_IMAGE_JAVA_PLATFORM_MODULE_SYSTEM";
+    static boolean modulePathBuild = Boolean.parseBoolean(System.getenv().get(ENV_VAR_USE_MODULE_SYSTEM));
 
     private static final String DEFAULT_GENERATOR_CLASS_NAME = NativeImageGeneratorRunner.class.getName();
     private static final String DEFAULT_GENERATOR_MODULE_NAME = ModuleSupport.getModuleName(NativeImageGeneratorRunner.class);
@@ -366,20 +366,7 @@ public class NativeImage {
          * @return additional arguments for JVM that runs image builder
          */
         default List<String> getBuilderJavaArgs() {
-            String javaVersion = String.valueOf(JavaVersionUtil.JAVA_SPEC);
-            String[] flagsForVersion = graalCompilerFlags.get(javaVersion);
-            if (flagsForVersion == null) {
-                String suffix = "";
-                if (System.getProperty("java.home").contains("-dev")) {
-                    suffix = " Update SubstrateCompilerFlagsBuilder.compute_graal_compiler_flags_map() in " +
-                                    "mx_substratevm.py to add a configuration for a new Java version.";
-                }
-                showError(String.format("Image building not supported for Java version %s in %s with VM configuration \"%s\".%s",
-                                System.getProperty("java.version"),
-                                System.getProperty("java.home"),
-                                System.getProperty("java.vm.name"),
-                                suffix));
-            }
+            ArrayList<String> builderJavaArgs = new ArrayList<>();
 
             if (useJVMCINativeLibrary == null) {
                 useJVMCINativeLibrary = false;
@@ -413,13 +400,35 @@ public class NativeImage {
                 }
             }
 
-            ArrayList<String> builderJavaArgs = new ArrayList<>();
-            builderJavaArgs.addAll(Arrays.asList(flagsForVersion));
             if (useJVMCINativeLibrary) {
                 builderJavaArgs.add("-XX:+UseJVMCINativeLibrary");
             } else {
                 builderJavaArgs.add("-XX:-UseJVMCICompiler");
             }
+
+            String javaVersion = String.valueOf(JavaVersionUtil.JAVA_SPEC);
+            String[] flagsForVersion = graalCompilerFlags.get(javaVersion);
+            if (flagsForVersion == null) {
+                String suffix = "";
+                if (System.getProperty("java.home").contains("-dev")) {
+                    suffix = " Update SubstrateCompilerFlagsBuilder.compute_graal_compiler_flags_map() in " +
+                                    "mx_substratevm.py to add a configuration for a new Java version.";
+                }
+                showError(String.format("Image building not supported for Java version %s in %s with VM configuration \"%s\".%s",
+                                System.getProperty("java.version"),
+                                System.getProperty("java.home"),
+                                System.getProperty("java.vm.name"),
+                                suffix));
+            }
+
+            for (String line : flagsForVersion) {
+                if (!modulePathBuild && line.startsWith("--add-exports=")) {
+                    builderJavaArgs.add(line.substring(0, line.lastIndexOf('=') + 1) + "ALL-UNNAMED");
+                } else {
+                    builderJavaArgs.add(line);
+                }
+            }
+
             return builderJavaArgs;
         }
 
@@ -545,7 +554,7 @@ public class NativeImage {
 
         @Override
         public List<Path> getBuilderClasspath() {
-            if (USE_NI_JPMS) {
+            if (modulePathBuild) {
                 return Collections.emptyList();
             }
             List<Path> result = new ArrayList<>();
@@ -604,7 +613,7 @@ public class NativeImage {
         @Override
         public List<Path> getBuilderModulePath() {
             List<Path> result = new ArrayList<>();
-            if (USE_NI_JPMS) {
+            if (modulePathBuild) {
                 result.addAll(getJars(rootDir.resolve(Paths.get("lib", "svm", "builder"))));
             } else {
                 result.addAll(getJars(rootDir.resolve(Paths.get("lib", "jvmci")), "graal-sdk", "enterprise-graal"));
@@ -858,7 +867,6 @@ public class NativeImage {
     }
 
     private void prepareImageBuildArgs() {
-        config.getBuilderJavaArgs().forEach(this::addImageBuilderJavaArgs);
         addImageBuilderJavaArgs("-Xss10m");
         addImageBuilderJavaArgs(oXms + getXmsValue());
         String xmxVal = getXmxValue(1);
@@ -887,32 +895,6 @@ public class NativeImage {
          * non-system class and and triggers a warning.
          */
         addImageBuilderJavaArgs("-Xshare:off");
-        config.getBuilderClasspath().forEach(this::addImageBuilderClasspath);
-
-        if (config.getBuilderInspectServerPath() != null) {
-            addPlainImageBuilderArg(oHInspectServerContentPath + config.getBuilderInspectServerPath());
-        }
-
-        if (config.useJavaModules()) {
-            config.getBuilderModulePath().forEach(this::addImageBuilderModulePath);
-            String upgradeModulePath = config.getBuilderUpgradeModulePath().stream()
-                            .map(p -> canonicalize(p).toString())
-                            .collect(Collectors.joining(File.pathSeparator));
-            if (!upgradeModulePath.isEmpty()) {
-                addImageBuilderJavaArgs(Arrays.asList("--upgrade-module-path", upgradeModulePath));
-            }
-        } else {
-            config.getBuilderJVMCIClasspath().forEach((Consumer<? super Path>) this::addImageBuilderClasspath);
-            if (!config.getBuilderJVMCIClasspathAppend().isEmpty()) {
-                String builderJavaArg = config.getBuilderJVMCIClasspathAppend()
-                                .stream().map(path -> canonicalize(path).toString())
-                                .collect(Collectors.joining(File.pathSeparator, "-Djvmci.class.path.append=", ""));
-                addImageBuilderJavaArgs(builderJavaArg);
-            }
-
-            config.getBuilderBootClasspath().forEach((Consumer<? super Path>) this::addImageBuilderBootClasspath);
-            config.getResourcesJar().ifPresent(this::addImageBuilderClasspath);
-        }
 
         config.getImageClasspath().forEach(this::addCustomImageClasspath);
     }
@@ -1089,6 +1071,33 @@ public class NativeImage {
     private int completeImageBuild() {
         List<String> leftoverArgs = processNativeImageArgs();
 
+        config.getBuilderClasspath().forEach(this::addImageBuilderClasspath);
+
+        if (config.getBuilderInspectServerPath() != null) {
+            addPlainImageBuilderArg(oHInspectServerContentPath + config.getBuilderInspectServerPath());
+        }
+
+        if (config.useJavaModules()) {
+            config.getBuilderModulePath().forEach(this::addImageBuilderModulePath);
+            String upgradeModulePath = config.getBuilderUpgradeModulePath().stream()
+                            .map(p -> canonicalize(p).toString())
+                            .collect(Collectors.joining(File.pathSeparator));
+            if (!upgradeModulePath.isEmpty()) {
+                addImageBuilderJavaArgs(Arrays.asList("--upgrade-module-path", upgradeModulePath));
+            }
+        } else {
+            config.getBuilderJVMCIClasspath().forEach((Consumer<? super Path>) this::addImageBuilderClasspath);
+            if (!config.getBuilderJVMCIClasspathAppend().isEmpty()) {
+                String builderJavaArg = config.getBuilderJVMCIClasspathAppend()
+                                .stream().map(path -> canonicalize(path).toString())
+                                .collect(Collectors.joining(File.pathSeparator, "-Djvmci.class.path.append=", ""));
+                addImageBuilderJavaArgs(builderJavaArg);
+            }
+
+            config.getBuilderBootClasspath().forEach((Consumer<? super Path>) this::addImageBuilderBootClasspath);
+            config.getResourcesJar().ifPresent(this::addImageBuilderClasspath);
+        }
+
         completeOptionArgs();
         addTargetArguments();
 
@@ -1168,7 +1177,7 @@ public class NativeImage {
                 boolean hasMainClass = mainClass != null && !mainClass.isEmpty();
                 if (extraImageArgs.isEmpty()) {
                     if (buildExecutable && !hasMainClassModule && !hasMainClass) {
-                        String moduleMsg = USE_NI_JPMS ? " (or <module>/<mainclass>)" : "";
+                        String moduleMsg = modulePathBuild ? " (or <module>/<mainclass>)" : "";
                         showError("Please specify class" + moduleMsg + " containing the main entry point method. (see --help)");
                     }
                 } else if (!moduleOptionMode) {
@@ -1222,7 +1231,7 @@ public class NativeImage {
         finalImageClasspath.addAll(imageClasspath);
 
         List<Path> imageProvidedJars;
-        if (USE_NI_JPMS) {
+        if (modulePathBuild) {
             imageProvidedJars = config.getImageProvidedModulePath();
             finalImageModulePath.addAll(imageProvidedJars);
         } else {
@@ -1235,7 +1244,9 @@ public class NativeImage {
             /* Bypass regular build and proceed with fallback image building */
             return 2;
         }
-        return buildImage(imageBuilderJavaArgs, imageBuilderBootClasspath, imageBuilderClasspath, imageBuilderModulePath, imageBuilderArgs, finalImageClasspath, finalImageModulePath);
+
+        List<String> finalImageBuilderJavaArgs = Stream.concat(config.getBuilderJavaArgs().stream(), imageBuilderJavaArgs.stream()).collect(Collectors.toList());
+        return buildImage(finalImageBuilderJavaArgs, imageBuilderBootClasspath, imageBuilderClasspath, imageBuilderModulePath, imageBuilderArgs, finalImageClasspath, finalImageModulePath);
     }
 
     private static String getLocationAgnosticArgPrefix(String argPrefix) {
@@ -1411,7 +1422,7 @@ public class NativeImage {
             arguments.addAll(strings);
         }
 
-        if (USE_NI_JPMS) {
+        if (modulePathBuild) {
             arguments.addAll(Arrays.asList("--module", DEFAULT_GENERATOR_MODULE_NAME + "/" + DEFAULT_GENERATOR_CLASS_NAME));
         } else {
             arguments.add(config.getGeneratorMainClass());
@@ -1457,6 +1468,9 @@ public class NativeImage {
         try {
             ProcessBuilder pb = new ProcessBuilder();
             pb.command(command);
+            if (modulePathBuild) {
+                pb.environment().put(ENV_VAR_USE_MODULE_SYSTEM, Boolean.toString(true));
+            }
             p = pb.inheritIO().start();
             exitStatus = p.waitFor();
         } catch (IOException | InterruptedException e) {
@@ -1656,6 +1670,7 @@ public class NativeImage {
             return;
         }
 
+        modulePathBuild = true;
         imageModulePath.add(mpEntry);
         processClasspathNativeImageMetaInf(mpEntry);
     }
@@ -1728,6 +1743,7 @@ public class NativeImage {
 
     void setModuleOptionMode(boolean val) {
         moduleOptionMode = val;
+        modulePathBuild = true;
     }
 
     boolean isVerbose() {

compiler/src/org.graalvm.compiler.options.jdk11/src/org/graalvm/compiler/options/ModuleSupport.java
@@ -28,7 +28,7 @@ import java.util.ServiceLoader;
 
 public class ModuleSupport {
 
-    public static final boolean USE_NI_JPMS = System.getenv().getOrDefault("USE_NATIVE_IMAGE_JAVA_PLATFORM_MODULE_SYSTEM", "false").toLowerCase().equals("true");
+    public static final boolean USE_NI_JPMS = Boolean.parseBoolean(System.getenv().get("USE_NATIVE_IMAGE_JAVA_PLATFORM_MODULE_SYSTEM"));
 
     static Iterable<OptionDescriptors> getOptionsLoader() {
         /*

substratevm/mx.substratevm/mx_substratevm.py
@@ -60,8 +60,6 @@ if sys.version_info[0] < 3:
 else:
     from io import StringIO
 
-USE_NI_JPMS = os.environ.get('USE_NATIVE_IMAGE_JAVA_PLATFORM_MODULE_SYSTEM', 'false').lower() == 'true'
-
 suite = mx.suite('substratevm')
 svmSuites = [suite]
 
@@ -71,13 +69,19 @@ def svm_java_compliance():
 def svm_java8():
     return svm_java_compliance() <= mx.JavaCompliance('1.8')
 
-def graal_compiler_flags(all_unnamed=True):
+def graal_compiler_flags():
     version_tag = svm_java_compliance().value
-    compiler_flags = mx.dependency('substratevm:svm-compiler-flags-builder').compute_graal_compiler_flags_map(all_unnamed=all_unnamed)
+    compiler_flags = mx.dependency('substratevm:svm-compiler-flags-builder').compute_graal_compiler_flags_map()
     if version_tag not in compiler_flags:
         missing_flags_message = 'Missing graal-compiler-flags for {0}.\n Did you forget to run "mx build"?'
         mx.abort(missing_flags_message.format(version_tag))
-    return compiler_flags[version_tag]
+    def adjusted_exports(line):
+        if line.startswith('--add-exports='):
+            before, sep, _ = line.rpartition('=')
+            return before + sep + 'ALL-UNNAMED'
+        else:
+            return line
+    return [adjusted_exports(line) for line in compiler_flags[version_tag]]
 
 def svm_unittest_config_participant(config):
     vmArgs, mainClass, mainClassArgs = config
@@ -839,7 +843,7 @@ mx_sdk_vm.register_graalvm_component(mx_sdk_vm.GraalVmJreComponent(
     support_distributions=['substratevm:NATIVE_IMAGE_GRAALVM_SUPPORT'],
     launcher_configs=[
         mx_sdk_vm.LauncherConfig(
-            use_modules='image' if USE_NI_JPMS else 'launcher' if not svm_java8() else None,
+            use_modules='image' if not svm_java8() else None,
             main_module="org.graalvm.nativeimage.driver",
             destination="bin/<exe:native-image>",
             jar_distributions=["substratevm:SVM_DRIVER"],
@@ -850,7 +854,7 @@ mx_sdk_vm.register_graalvm_component(mx_sdk_vm.GraalVmJreComponent(
     ],
     library_configs=[
         mx_sdk_vm.LibraryConfig(
-            use_modules='image' if USE_NI_JPMS else 'launcher' if not svm_java8() else None,
+            use_modules='image' if not svm_java8() else None,
             destination="<lib:native-image-agent>",
             jvm_library=True,
             jar_distributions=[
@@ -864,7 +868,7 @@ mx_sdk_vm.register_graalvm_component(mx_sdk_vm.GraalVmJreComponent(
             ],
         ),
         mx_sdk_vm.LibraryConfig(
-            use_modules='image' if USE_NI_JPMS else 'launcher' if not svm_java8() else None,
+            use_modules='image' if not svm_java8() else None,
             destination="<lib:native-image-diagnostics-agent>",
             jvm_library=True,
             jar_distributions=[
@@ -1017,7 +1021,7 @@ mx_sdk_vm.register_graalvm_component(mx_sdk_vm.GraalVmJreComponent(
     support_distributions=[],
     launcher_configs=[
         mx_sdk_vm.LauncherConfig(
-            use_modules='image' if USE_NI_JPMS else 'launcher' if not svm_java8() else None,
+            use_modules='image' if not svm_java8() else None,
             main_module="org.graalvm.nativeimage.configure",
             destination="bin/<exe:native-image-configure>",
             jar_distributions=["substratevm:SVM_CONFIGURE"],
@@ -1088,8 +1092,6 @@ def hellomodule(args):
     """
     if svm_java8():
         mx.abort('Experimental module support requires Java 11+')
-    if os.environ.get('USE_NATIVE_IMAGE_JAVA_PLATFORM_MODULE_SYSTEM', 'false') != 'true':
-        mx.abort('Experimental module support requires USE_NATIVE_IMAGE_JAVA_PLATFORM_MODULE_SYSTEM=true for "mx build" and "mx hellomodule"')
 
     # Build a helloworld Java module with maven
     module_path = []
@@ -1440,7 +1442,7 @@ class SubstrateCompilerFlagsBuilder(mx.ArchivableProject):
 
     # If renaming or moving this method, please update the error message in
     # com.oracle.svm.driver.NativeImage.BuildConfiguration.getBuilderJavaArgs().
-    def compute_graal_compiler_flags_map(self, all_unnamed=not USE_NI_JPMS):
+    def compute_graal_compiler_flags_map(self):
         graal_compiler_flags_map = dict()
         graal_compiler_flags_map[8] = [
             '-d64',
@@ -1457,15 +1459,9 @@ class SubstrateCompilerFlagsBuilder(mx.ArchivableProject):
             distributions_transitive = mx.classpath_entries(self.deps)
             jdk = mx.get_jdk(tag='default')
             required_exports = mx_javamodules.requiredExports(distributions_transitive, jdk)
-            target_module = 'ALL-UNNAMED' if all_unnamed else None
-            exports_flags = mx_sdk_vm.AbstractNativeImageConfig.get_add_exports_list(required_exports, target_module)
+            exports_flags = mx_sdk_vm.AbstractNativeImageConfig.get_add_exports_list(required_exports)
             graal_compiler_flags_map[11].extend(exports_flags)
-
-            # Currently JDK 13, 14, 15 and JDK 11 have the same flags
-            graal_compiler_flags_map[13] = graal_compiler_flags_map[11]
-            graal_compiler_flags_map[14] = graal_compiler_flags_map[11]
-            graal_compiler_flags_map[15] = graal_compiler_flags_map[11]
-            graal_compiler_flags_map[16] = graal_compiler_flags_map[11]
+            # Currently JDK 17 and JDK 11 have the same flags
             graal_compiler_flags_map[17] = graal_compiler_flags_map[11]
             # DO NOT ADD ANY NEW ADD-OPENS OR ADD-EXPORTS HERE!
             #

commit ae7dde07ea20b41b1971cc2a769d38246e45c84b

Non-jlinked-build fixes for breakage after cc11ae1
Only perform nashorn substitutions if that module is actulally loaded as
it would otherwise affect JDK 9+ builds.

cc11ae1d1fad550424b1fdc0a6cf7e9d6815874a re-ordered arguments passed to
the native image generator. Add -XX:+UnlockExperimentalVMOptions before
actually using any one of those experimental options as that's needed
for some JVMs.

Add relevant modules to the upgraded module path list since for
non-jlinked-builds they are either not there or too old.

Closes #3778

substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jdk/NashornSupport.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -22,11 +22,28 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
+
 package com.oracle.svm.core.jdk;
 
-import com.oracle.svm.core.annotate.TargetClass;
-import com.oracle.svm.core.jdk.NashornSupport.NashornAvailable;
+import java.util.function.BooleanSupplier;
 
-@TargetClass(className = "jdk.nashorn.api.scripting.ClassFilter", onlyWith = {JDK14OrEarlier.class, NashornAvailable.class})
-public final class Target_jdk_nashorn_api_scripting_ClassFilter {
+public final class NashornSupport {
+
+    // Determine if the jdk.scripting.nashorn module (or jar) is available. If it's not,
+    // the boolean supplier should return false.
+    static class NashornAvailable implements BooleanSupplier {
+
+        private static final String CLASSFILTER_NAME = "jdk.nashorn.api.scripting.ClassFilter";
+
+        @Override
+        public boolean getAsBoolean() {
+            try {
+                Class.forName(CLASSFILTER_NAME);
+                return true;
+            } catch (ClassNotFoundException e) {
+                return false;
+            }
+        }
+
+    }
 }

substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jdk/Target_jdk_nashorn_api_scripting_NashornScriptEngineFactory.java
@@ -28,9 +28,10 @@ import javax.script.ScriptEngine;
 
 import com.oracle.svm.core.annotate.Substitute;
 import com.oracle.svm.core.annotate.TargetClass;
+import com.oracle.svm.core.jdk.NashornSupport.NashornAvailable;
 import com.oracle.svm.core.util.VMError;
 
-@TargetClass(className = "jdk.nashorn.api.scripting.NashornScriptEngineFactory", onlyWith = JDK14OrEarlier.class)
+@TargetClass(className = "jdk.nashorn.api.scripting.NashornScriptEngineFactory", onlyWith = {JDK14OrEarlier.class, NashornAvailable.class})
 public final class Target_jdk_nashorn_api_scripting_NashornScriptEngineFactory {
     @Substitute
     @SuppressWarnings({"unused", "static-method"})

substratevm/src/com.oracle.svm.driver/src/com/oracle/svm/driver/NativeImage.java
@@ -106,8 +106,9 @@ public class NativeImage {
         return (OS.getCurrent().className + "-" + SubstrateUtil.getArchitectureName()).toLowerCase();
     }
 
+    private static final String DEFAULT_COMMUNITY_EDITION = "CE";
     static final String graalvmVersion = System.getProperty("org.graalvm.version", "dev");
-    static final String graalvmConfig = System.getProperty("org.graalvm.config", "CE");
+    static final String graalvmConfig = System.getProperty("org.graalvm.config", DEFAULT_COMMUNITY_EDITION);
 
     private static Map<String, String[]> getCompilerFlags() {
         Map<String, String[]> result = new HashMap<>();
@@ -484,6 +485,8 @@ public class NativeImage {
             if (useJVMCINativeLibrary) {
                 builderJavaArgs.add("-XX:+UseJVMCINativeLibrary");
             } else {
+                // some JVMs need this unlocking
+                builderJavaArgs.add("-XX:+UnlockExperimentalVMOptions");
                 builderJavaArgs.add("-XX:-UseJVMCICompiler");
             }
 
@@ -537,7 +540,14 @@ public class NativeImage {
          * @return entries for the --upgrade-module-path of the image builder
          */
         public List<Path> getBuilderUpgradeModulePath() {
-            return getJars(rootDir.resolve(Paths.get("lib", "jvmci")), "graal", "graal-management");
+            List<Path> result = new ArrayList<>();
+            result.addAll(getJars(rootDir.resolve(Paths.get("lib", "jvmci")), "graal", "graal-management"));
+            // Non-jlinked JDKs need to upgrade the module path
+            if (!DEFAULT_COMMUNITY_EDITION.equals(graalvmConfig)) {
+                result.addAll(getJars(rootDir.resolve(Paths.get("lib", "jvmci")), "graal-sdk", "enterprise-graal"));
+                result.addAll(getJars(rootDir.resolve(Paths.get("lib", "truffle")), "truffle-api"));
+            }
+            return result;
         }
 
         /**